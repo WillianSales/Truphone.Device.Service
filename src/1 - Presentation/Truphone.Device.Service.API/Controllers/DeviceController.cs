a287da41cadd8b78b5cce7fb66c316a1ef445e42
willian.sales <willian.o.sales@hotmail.com>
2022-01-04 17:59:51 +0000

    Project created

---
+++ src/1 - Presentation/Truphone.Device.Service.API/Controllers/DeviceController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Truphone.Device.Service.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DeviceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
